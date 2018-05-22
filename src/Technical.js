d0437372db4f4167c1a59203fa7df8bcb9659f77
Jafer <itsjafer@gmail.com>
2018-05-22 18:34:17 -0400

    Added gitignore. Fixed typo

--- src/Technical.js
+++ src/Technical.js
@@ -14,13 +14,13 @@ class Technical extends Component {
         <p> <i>Current goals</i>: learn Flutter to create a mobile app. </p>
         <h3>On-the-job</h3>
         <ul>
-          <li><b>Axes Blotter</b> - a desktop application written in C# using MVVM that allows Bond traders at BMO Capital Markets to create, manage, and publish discounted bonds to clientele</li>
+          <li><b>Axes Blotter</b> - a desktop application written in C# using MVVM that allows bond traders at BMO Capital Markets to create, manage, and publish discounted bonds to clientele</li>
           <li><b>Bulk Trade Entry System</b> - functionality written in C# that allows several trades to be generated at once and published to a RabbitMQ back-end to allow quicker workflows</li>
           <li><b>Prospect</b> - a web dashboard written in Node.js with a PowerShell backend used by DevOps and FieldOps teams to remotely monitor, update, and deploy to clients</li>
         </ul>
         <h3>Miscellaneous</h3>
         <ul>
-          <li>I like to use Linux as my pesonal main operating system although I've overwhelmingly used Windows in my professional life</li>
+          <li>I like to use Linux as my personal main operating system although I've overwhelmingly used Windows in my professional life</li>
           <li>I've got a <i>Bloomberg Market Concepts</i> certification using a Bloomberg Terminal</li>
           <li>In my freshman year, I reached the semi-finals of the BDO New Venture competition where contestants pitched ideas for new business ventures.</li>
         </ul>
