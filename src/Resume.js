aabe052405942b71f1ed71b7353cc89cc1e74a2f
Jafer <itsjafer@gmail.com>
2018-07-27 11:54:28 -0400

    Updated resume with link to Overleaf

--- src/Resume.js
+++ src/Resume.js
@@ -6,6 +6,7 @@ class Resume extends Component {
   render() {
     return (
       <div>
+        <p> My resume was written in LaTex. To view the source code and the PDF side by side, you can view it directly on Overleaf <a href="https://v2.overleaf.com/read/wvmqpzrthcpw">here</a>. </p>
         <p> For convenience, here's a <a href={ResumePDF}>direct link</a> to my resume. </p>
         <PDF file={ResumePDF} pageNumber={1} className="pdf" fillWidth/>
       </div>
