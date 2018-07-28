5934e9bd3d46d2e0ddfed37c4a9a69cb94d9794a
Jafer <itsjafer@gmail.com>
2018-07-28 19:51:09 -0400

    Centered navbar, updated technical page, resume now uses direct link from git

--- src/Resume.js
+++ src/Resume.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Document, Page } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
 import PDF from 'react-pdf-js';
 import ResumePDF from './resume.pdf';
 
@@ -7,8 +9,12 @@ class Resume extends Component {
     return (
       <div>
         <p> My resume was written in LaTeX. To view the source code and the PDF side by side, you can view it directly on Overleaf <a href="https://v2.overleaf.com/read/wvmqpzrthcpw">here</a>. </p>
-        <p> For convenience, here's a <a href={ResumePDF}>direct link</a> to my resume. </p>
-        <PDF file={ResumePDF} pageNumber={1} className="pdf" fillWidth/>
+        <p> For convenience, here is a <a href={ResumePDF}>direct link</a> to my resume. </p>
+        <Document 
+        file="https://github.com/itsjafer/Resume/raw/master/Resume.pdf" 
+        >
+          <Page pageNumber={1} renderTextLayer={false}/>
+        </Document>
       </div>
     );
   }
--- src/Technical.js
+++ src/Technical.js
@@ -12,10 +12,11 @@ class Technical extends Component {
           <li><a href="http://github.com/itsjafer/sorcery"><b>sorcery</b></a> - an object-oriented implementation of a card game based on Magic: The Gathering and Hearthstone written in C++ </li>
           <li><b>C++/WLP4 Compiler</b> - A complete implementation of a compiler of a C++ subset called WLP4; converts WLP4 code to MIPS to binary machine code completely. </li>
         </ul>
-        <p> <i>Current goals</i>: learn Flutter to create a mobile app. </p>
+        <p> <i>Current goals</i>: learn Flutter to create a mobile app. Create a web app using Ruby on Rails. </p>
         <h3>On-the-job</h3>
         <ul>
           <li><b>Axes Blotter</b> - a desktop application written in C# using MVVM that allows bond traders at BMO Capital Markets to create, manage, and publish discounted bonds to clientele</li>
+          <li><b>Bond TradeHistory</b> - a feature for a WPF application that allowed traders to view the sales history of bonds located in a database in Oracle SQL.</li>
           <li><b>Bulk Trade Entry System</b> - functionality written in C# that allows several trades to be generated at once and published to a RabbitMQ back-end to allow quicker workflows</li>
           <li><b>Prospect</b> - a web dashboard written in Node.js with a PowerShell backend used by DevOps and FieldOps teams to remotely monitor, update, and deploy to clients</li>
         </ul>
@@ -23,7 +24,7 @@ class Technical extends Component {
         <ul>
           <li>I like to use Linux as my personal main operating system although I've overwhelmingly used Windows in my professional life</li>
           <li>I've got a <i>Bloomberg Market Concepts</i> certification using a Bloomberg Terminal</li>
-          <li>In my freshman year, I reached the semi-finals of the BDO New Venture competition where contestants pitched ideas for new business ventures.</li>
+          <li>In my freshman year, I reached the semi-finals of the BDO New Venture competition where contestants pitched ideas for new business ventures. My idea was a U-lock for skateboards called the BoardLock.</li>
         </ul>
       </div>
     );
