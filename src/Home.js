b8c87c9acc06a98642783daeda0c3dca3afbeaa4
Jafer <itsjafer@gmail.com>
2018-07-29 19:33:09 -0400

    Updated home welcome text

--- src/Home.js
+++ src/Home.js
@@ -5,14 +5,15 @@ class Home extends Component {
     return (
       <div>
         <h4>Hi there,</h4>
-        <p>I'm a student studying concurrently at the University of Waterloo and Wilfrid Laurier University working towards an <b>Honours Bachelor of Computer Science</b> and an <b>Honours Bachelor of Business Administration</b> respectively. </p>
+        <p>I'm a student studying concurrently at the University of Waterloo and Wilfrid Laurier University working towards an <b>Honours Bachelor of Computer Science</b> and an <b>Honours Bachelor of Business Administration</b> respectively expecting to graduate in 2021 with a concentration in <b>finance</b> and a concentration in <b>software engineering</b>. </p>
  
-        <p>I will graduate in 2021 with five four-month internships. So far, I've worked as a <b>DevOps Engineer</b> at Camis inc. and a <b>Software Developer</b> at BMO Capital Markets. </p>
+        <p>Upon graduation, I will have five internships of four months each under my belt. So far, I've worked as a <b>DevOps Engineer</b> at Camis inc. and a <b>Software Developer</b> at BMO Capital Markets. </p>
 
-        <p>I'm a <b>Canadian citizen</b>, having grown up in the Greater Toronto Area, as well as a <b>permanent resident of the United States</b>, willing to work anywhere in the world that will offer me a great learning experience and a chance to better myself and others. My legal name is <b>Syed Jafer Haider</b>. </p>
+        <p>My legal name is <b>Syed Jafer Haider</b>. I am a <b>Canadian citizen</b>, having grown up in the Greater Toronto Area, as well as a <b>permanent resident of the United States</b>, willing to work anywhere in the world that will offer me a great learning experience and a chance to better myself and others.</p>
 
-        <p>When I'm not coding, you'll find me eagerly awaiting the next season of Game of Thrones, rewatching The Office for the 10th time, trying a new Shawarma place, or playing Rocket League/Fortnite/FIFA with my friends. I <i>love</i> playing board games; my favourites right now are Coup and The Resistance. In the summer, I like to coach house-league soccer.</p>
+        <p>When I'm not coding, you'll find me eagerly awaiting the next season of Game of Thrones, rewatching The Office for the 10th time, trying a new shawarma place, or playing Rocket League, God of War, Fortnite, and FIFA. I <i>love</i> playing board games; my favourites right now are Coup and The Resistance. In the summer, I like to coach house-league soccer as a way to stay active and work on my leadership skills.</p>
 
+        <p>I invite you to explore my website to learn more about my experience. Feel free to contact me at my email or any of my social media listed above!</p>
        </div>
     );
   }
