9e332646b373f17f2fcd60d77560813d9afaed00
Jafer <itsjafer@gmail.com>
2018-07-28 20:28:29 -0400

    Updated text to look pretty

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
  
         <p>I will graduate in 2021 with five four-month internships. So far, I've worked as a <b>DevOps Engineer</b> at Camis inc. and a <b>Software Developer</b> at BMO Capital Markets. </p>
 
-        <p>I'm a <b>Canadian citizen</b> and a <b>permanent resident of the United States</b>, willing to work anywhere in the world that will offer me a great learning experience and a chance to better myself and others. </p>
+        <p>I'm a <b>Canadian citizen</b>, having grown up in the Greater Toronto Area, as well as a <b>permanent resident of the United States</b>, willing to work anywhere in the world that will offer me a great learning experience and a chance to better myself and others. My legal name is <b>Syed Jafer Haider</b>. </p>
 
         <p>When I'm not coding, you'll find me eagerly awaiting the next season of Game of Thrones, rewatching The Office for the 10th time, trying a new Shawarma place, or playing Rocket League/Fortnite/FIFA with my friends. I <i>love</i> playing board games; my favourites right now are Coup and The Resistance. In the summer, I like to coach house-league soccer.</p>
 
