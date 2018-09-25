595c0185c46a657d1be5bf5798647cface2bdb5c
Jafer <itsjafer@gmail.com>
2018-09-25 00:32:41 -0400

    Updated home page with current job and added new resume

--- src/Resume.js
+++ src/Resume.js
@@ -10,7 +10,7 @@ class Resume extends Component {
         <p> My resume was written in LaTeX. To view the source code and the PDF side by side, you can view it directly on Overleaf <a href="https://v2.overleaf.com/read/wvmqpzrthcpw" target="_blank" rel="noopener noreferrer">here</a>. </p>
         <p> For convenience, here is a <a href={ResumePDF} target="_blank">direct link</a> to my resume. </p>
         <Document file={ResumePDF} renderMode="svg">
-          <Page pageNumber={1} renderTextLayer={true} scale={1.5}/>
+          <Page pageNumber={1} renderTextLayer={false} scale={1.5}/>
         </Document>
       </div>
     );
