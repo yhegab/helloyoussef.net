9d4936821dbb5a5e0edbf53a30e2728a6b5fb856
Jafer <itsjafer@gmail.com>
2018-10-24 13:27:46 -0400

    Updated with extracurriculars

--- src/Home.js
+++ src/Home.js
@@ -8,8 +8,10 @@ class Home extends Component {
 
         <p>My name is <b>Syed Jafer Haider</b> and I'm a third-year student studying concurrently at the University of Waterloo and Wilfrid Laurier University working towards an <b>Honours Bachelor of Computer Science</b> and an <b>Honours Bachelor of Business Administration</b> respectively expecting to graduate in 2021 with a concentration in <b>Finance</b> and a minor in <b>French</b>.</p>
  
-        <p>Currently, I am working as a <b>Mobile Developer</b> at National Logistics Services during the Fall 2018 term. Before that, I've worked as a <b>Software Developer</b> at BMO Capital Markets during the Winter 2018 term and a <b>DevOps Engineer</b> at Camis inc. during the Summer 2017 term.</p>
+        <p>Currently, I am working as a <b>Technical Product Manager</b> at National Logistics Services during the Fall 2018 term. Before that, I've worked as a <b>Software Developer</b> at BMO Capital Markets during the Winter 2018 term and a <b>DevOps Engineer</b> at Camis inc. during the Summer 2017 term.</p>
 
+        <p>I'm the <b>Lead Consultant</b> at the Laurier Consulting Program where I work with <a href="https://www.gimme360.ca/">GIMME360</a>, a virtual reality start up, as our client. I'm also a member of the Waterloo Data Science Club and the Product Vision Club which focus on developing skills in data science and product management, respectively.</p>
+        
         <p>I am a <b>Canadian citizen</b>, having grown up in the Greater Toronto Area, as well as a <b>permanent resident of the United States</b>, willing to work anywhere in the world that will offer me a great learning experience and a chance to better myself and others.</p>
 
         <p>I invite you to explore my website to learn more about my experience. Feel free to contact me at my email or any of my social media listed above!</p>
