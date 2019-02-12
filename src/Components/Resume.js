76da185602fdd386bce3afe2e5aa9a6642f8d747
Jafer <itsjafer@gmail.com>
2019-02-11 23:01:52 -0500

    Update travis

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -14,7 +14,7 @@ class Resume extends Component {
     return (
       <div className="Resume">
         <p> My resume was written in LaTeX using OverLeaf. You can see the latest version below.</p>
-        <p> For convenience, here is a <a href={"Resume.pdf"} target="_blank">direct link</a> to my resume. </p>
+        <p> For convenience, here is a <a href={"Resume.pdf"} target="_blank" rel="noopener noreferrer">direct link</a> to my resume. </p>
         
         <Document file={"Resume.pdf"} renderMode="canvas">
           <Page pageNumber={1} renderMode="canvas" renderTextLayer={true} scale={1.5}/>
