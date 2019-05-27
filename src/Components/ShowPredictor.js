ca3da66adc38eb075d930ec300e459cec2edf05c
Jafer Haider <itsjafer@Jafers-MacBook-Pro.local>
2019-05-27 03:41:01 -0700

    Added conditional rendering for recommendations

--- src/Components/ShowPredictor.js
+++ src/Components/ShowPredictor.js
@@ -5,10 +5,28 @@ import 'react-pdf/dist/Page/AnnotationLayer.css';
 class ShowPredictor extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: '', data: []};
+    this.state = {
+        value: '', 
+        data: [],
+        isDesktop: false
+    };
 
+    this.updatePredicate = this.updatePredicate.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.updatePredicate();
+    window.addEventListener("resize", this.updatePredicate);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updatePredicate);
+  }
+
+  updatePredicate() {
+    this.setState({ isDesktop: window.innerWidth > 1250 });
   }
 
   handleChange(event) {
@@ -24,6 +42,8 @@ class ShowPredictor extends Component {
   }
 
   render() {
+    const isDesktop = this.state.isDesktop;
+    
     return (
       <div className="show-predictor">
         <p> Enter in a TV Show and I will recommend 10 other TV Shows you might like based on their rating and similarity.</p>
@@ -43,7 +63,7 @@ class ShowPredictor extends Component {
         <div className="table">
         <table>
             <tbody>
-            {this.state.data.length > 0 &&
+            {this.state.data.length > 0 && isDesktop &&
             <tr>
                 <th>TV Show Title</th>
                 <th>Recommendation Score</th>
@@ -51,14 +71,25 @@ class ShowPredictor extends Component {
                 <th>Similarity Score</th>
             </tr>
             }
+            {this.state.data.length > 0 && !isDesktop &&
+            <tr>
+                <th>TV Show Title</th>
+                <th>Recommendation Score</th>
+            </tr>
+            }
             {this.state.data.map(function(item, key) {
-             
+                
                return (
+                isDesktop ?
                 <tr key = {key}>
                     <td>{item.title}</td>
                     <td>{(item.score * 100).toFixed(2)}%</td>
                     <td>{item.user_rating}</td>
                     <td>{(item.similarity * 100).toFixed(2)}%</td>
+                </tr> :
+                <tr key = {key}>
+                    <td>{item.title}</td>
+                    <td>{(item.score * 100).toFixed(2)}%</td>
                 </tr>
                 )
              
