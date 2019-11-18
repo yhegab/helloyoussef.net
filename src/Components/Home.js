d4bde9cb54202aeb8f87b1365b8a106e80276dfc
Jafer <itsjafer@gmail.com>
2019-11-18 17:38:09 -0500

    Update home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,15 +10,17 @@ class Home extends Component {
  
         <p>I am a <b>Canadian citizen</b> as well as a <b>permanent resident of the United States</b>, having grown up in the Greater Toronto Area and willing to work anywhere in the world that will offer me a great learning experience and a chance to better myself and others.</p>
         
-        <p>I have completed four internships and plan to do two more. I've tried to explore a different discipline within Computer Science with every experience:</p>
+        <p>I have completed one research assistantship and five internships. I've tried to explore a different discipline within Computer Science with every experience:</p>
         <ul>
-          <li><b>Data Scientist</b> Intern at Vida Health (May 2019 - August 2019, 4 months)</li>
+          <li><b>Software Engineer - Infrastructure</b> Intern at Yelp (January 2020 - April 2020, 4 months)</li>
+          <li><b>Research Assistant</b> at University of Waterloo (September 2019 - December 2019, 4 months)</li>
+          <li><b>Software Engineer - Data Science</b> Intern at Vida Health (May 2019 - August 2019, 4 months)</li>
           <li><b>Mobile Developer</b> Part-time Intern at National Logistics Services (September 2018 - December 2018, 4 months)</li>
           <li><b>Software Developer</b> Intern at BMO Capital Markets (January 2018 - April 2018, 4 months)</li>
           <li><b>DevOps Engineer</b> Intern at Camis Inc. (May 2017 - August 2017, 4 months)</li>
         </ul>
 
-        <p>I am also an <b>Undergraduate Research Assistant</b> under the guidance of Professor <a href="https://cs.uwaterloo.ca/people-profiles/peter-buhr">Peter Buhr</a> at the University of Waterloo. My work involves researching and implementing a set of atomic fence operations in <a href="https://cforall.uwaterloo.ca">C-for-all</a> to control compiler and  hardware code movement to prevent sequential code optimizations from invalidating concurrent programs.</p>
+        <p>I am currently looking for internship opportunities for Fall 2020 and Summer 2021.</p>
         
         <p>Apart from all that, I'm a big fan of board games, movies, and TV Shows (for real, I even have a <a href="/#/favourites">list</a>), I love a good shawarma, and I'm slowly learning how to swim!</p>
 
