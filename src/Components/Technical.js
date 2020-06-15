21c44f12de46f5ce87694b03e216b37fbb0eb59f
Jafer <itsjafer@gmail.com>
2020-06-14 20:52:12 -0500

    Added baby jafer and tried to cut down on verbiage

--- src/Components/Technical.js
+++ src/Components/Technical.js
@@ -8,14 +8,47 @@ class Technical extends Component {
           <p>
             {' '}
             Here is a
-            <a href="Resume.pdf" target="_blank" rel="noopener noreferrer"> direct link</a>
+            <a href="Resume.pdf" target="_blank" rel="noopener noreferrer"> direct link to my resume;</a>
             {' '}
-            to my resume; I am currently looking for internships and new grad opportunities!
+            I'm currently looking for internships and new grad opportunities!
             {' '}
           </p>
         </div>
-        <p>Other than that, here&apos;s a quick overview about my experience:</p>
-        <h3>Personal</h3>
+
+        <p>Here's a brief summary of my work experience:</p>
+        <ul>
+          <li>
+            <b>Software Engineer - Infrastructure</b>
+            {' '}
+            Intern at Yelp (January 2020 - April 2020, 4 months)
+          </li>
+          <li>
+            <b>Research Assistant</b>
+            {' '}
+            at University of Waterloo (September 2019 - December 2019, 4 months)
+          </li>
+          <li>
+            <b>Software Engineer - Data Science</b>
+            {' '}
+            Intern at Vida Health (May 2019 - August 2019, 4 months)
+          </li>
+          <li>
+            <b>Mobile Developer</b>
+            {' '}
+            Part-time Intern at National Logistics Services (September 2018 - December 2018, 4 months)
+          </li>
+          <li>
+            <b>Software Developer</b>
+            {' '}
+            Intern at BMO Capital Markets (January 2018 - April 2018, 4 months)
+          </li>
+          <li>
+            <b>DevOps Engineer</b>
+            {' '}
+            Intern at Camis Inc. (May 2017 - August 2017, 4 months)
+          </li>
+        </ul>
+        <p>Other than that, here&apos;s a few projects I'm particularly proud of:</p>
         <ul>
           <li>
             <a href="https://github.com/itsjafer/BetterMultitasking" target="_blank" rel="noopener noreferrer"><b>BetterMultitasking</b></a>
@@ -31,11 +64,6 @@ class Technical extends Component {
             <a href="https://github.com/itsjafer/BigPiPEnergy" target="_blank" rel="noopener noreferrer"><b>BigPiPEnergy</b></a>
             {' '}
             - iOS tweak that increases the Picture-in-Picture size limit
-          </li>
-          <li>
-            <a href="http://itsjafer.com" target="_blank" rel="noopener noreferrer"><b>This website</b></a>
-            {' '}
-            - developed using ReactJS with React Router.
           </li>
           <li>
             <a href="https://github.com/itsjafer/tv_recommendation_engine" target="_blank" rel="noopener noreferrer">
@@ -65,15 +93,6 @@ class Technical extends Component {
             - a personal messaging web app made using Angular, Mongo, Node.js, and Express and deployed using Heroku
           </li>
           <li>
-            <a href="http://github.com/itsjafer/sorcery" target="_blank" rel="noopener noreferrer"><b>sorcery</b></a>
-            {' '}
-            - an object-oriented implementation of a card game based on Magic: The Gathering and Hearthstone written in C++
-            {' '}
-          </li>
-        </ul>
-        <h3>On-the-job</h3>
-        <ul>
-          <li>
             <a href="https://github.com/itsjafer/sparkmonitor" target="_blank" rel="noopener noreferrer">
               <b>SparkMonitor</b>
               {' '}
@@ -82,64 +101,6 @@ class Technical extends Component {
             {' '}
             - an open-source JupyterLab extension to track progress of Spark jobs
           </li>
-          <li>
-            <b>Jupyterlab IAM Manager</b>
-            {' '}
-            @ Yelp - a JupyterLab extension that allows for management of AWS credentials from Jupyter itself
-          </li>
-          <li>
-            <b>Habit Recommendation Model</b>
-            {' '}
-            @ Vida Health - prediction pipeline built on scikit-learn, pandas, and BigQuery to recommend habits for users based on historical data
-          </li>
-          <li>
-            <b>Track and Trace</b>
-            {' '}
-            @ NLS - an ASP.NET web app that allows logistics clients to externally track shipments of cartons
-          </li>
-          <li>
-            <b>Visitor Management System</b>
-            {' '}
-            @ NLS - a cross-platform React Native iOS/Android application that manages visitors and employee sign in and out
-          </li>
-          <li>
-            <b>Fixed Income Permissions</b>
-            {' '}
-            @ BMO - a Razor MVC web application written in C# and JavaScript to manage permission to various tools at BMO
-          </li>
-          <li>
-            <b>Axes Blotter</b>
-            {' '}
-            @ BMO - a desktop application written in C# using MVVM that allows bond traders at BMO Capital Markets to create, manage, and publish discounted bonds to clientele
-          </li>
-          <li>
-            <b>Bond TradeHistory</b>
-            {' '}
-            @ BMO - a feature for a WPF application that allowed traders to view the sales history of bonds located in a database in Oracle SQL.
-          </li>
-          <li>
-            <b>Bulk Trade Entry System</b>
-            {' '}
-            @ BMO - functionality written in C# that allows several trades to be generated at once and published to a RabbitMQ back-end to allow quicker workflows
-          </li>
-          <li>
-            <b>Prospect</b>
-            {' '}
-            @ Camis - a web dashboard written in Node.js, jQuery with a PowerShell backend used by DevOps and FieldOps teams to remotely monitor, update, and deploy to clients
-          </li>
-        </ul>
-        <h3>Miscellaneous</h3>
-        <ul>
-          <li>In Fall 2019, I worked as an undergraduate research assistant at the University of Waterloo</li>
-          <li>I was the lead consultant at the Laurier Consulting Program in Fall 2018</li>
-          <li>I&apos;m learning to swim! I go to weekly swimming classes at the UW Aquatics Club</li>
-          <li>
-            I&apos;ve got a
-            <i> Bloomberg Market Concepts</i>
-            {' '}
-            certification using a Bloomberg Terminal
-          </li>
-          <li>In my freshman year, I reached the semi-finals of the BDO New Venture competition where contestants pitched ideas for new business ventures. My idea was a U-lock for skateboards called the BoardLock.</li>
         </ul>
       </div>
     );
