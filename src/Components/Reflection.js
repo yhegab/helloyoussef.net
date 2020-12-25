65e36aeea9674f3d6a646588ee31f089862eb6e0
Jafer <itsjafer@gmail.com>
2020-12-24 19:02:07 -0500

    Cleaning up the code a bit

--- src/Components/Reflection.js
+++ src/Components/Reflection.js
@@ -3,26 +3,17 @@ import MarkdownComponent from './MarkdownComponent';
 
 class Reflection extends Component {
   constructor(props) {
-    super(props);
     this.state = { file: this.props.file };
   }
 
-  async componentDidMount() {
-    // const result = await fetch(this.props.file);
-    // const text = await result.text();
-    // this.setState({ reflection: text })
-  }
 
   render() {
-    // console.log(this.state.file);
-
     return (
       <div className="reflection">
         <span className="reflectionTitle">{this.props.title}</span>
         <h4>{this.props.date}</h4>
         <MarkdownComponent markdownSrcPromise={this.state.file} />
         {' '}
-
       </div>
     );
   }
--- src/Components/Technical.js
+++ src/Components/Technical.js
@@ -34,81 +34,18 @@ class Technical extends Component {
             Here is a
             <a href="Resume.pdf" target="_blank" rel="noopener noreferrer"> direct link to my resume;</a>
             {' '}
-            As a 2021 graduate, I'm currently looking for new grad opportunities!
+            Ahead of my graduation in 2021, I'm currently looking for new grad opportunities!
             {' '}
           </p>
 
-        <SizeMe>
-            {({ size }) => (
-              <Document file={"Resume.pdf"}>
-                <Page pageNumber={1}  onLoadSuccess={this.removeTextLayerOffset} width={size.width ? size.width : 1} />
-              </Document>
-            )}
-        </SizeMe>
-            </div>
-
-        {/* <p>Here are a few projects I'm particularly proud of:</p>
-        <ul>
-          <li>
-            <a href="#/show-predictor">
-              {' '}
-              <b>TV Show Recommendation Engine</b>
-              {' '}
-            </a>
-            {' '}
-            - a mathematical model that scrapes data off metacritic and imdb and uses cosine_similarities to predict and recommend TV Shows.
-          </li>
-          <li>
-            <a href="#/loonie">
-              <b>Loonie</b>
-              {' '}
-            </a>
-            {' '}
-            - a personal finance dashboard using Plaid API.
-          </li>
-          <li>
-            <a href="https://github.com/itsjafer/BetterMultitasking" target="_blank" rel="noopener noreferrer"><b>BetterMultitasking</b></a>
-            {' '}
-            - iOS tweak that enables iPads to run iPhone apps natively
-          </li>
-          <li>
-            <a href="https://github.com/itsjafer/StopShortcutsNotifications" target="_blank" rel="noopener noreferrer"><b>StopShortcutsNotifications</b></a>
-            {' '}
-            - iOS tweak that disables annoying shortcuts automation notifications
-          </li>
-          <li>
-            <a href="https://github.com/itsjafer/BigPiPEnergy" target="_blank" rel="noopener noreferrer"><b>BigPiPEnergy</b></a>
-            {' '}
-            - iOS tweak that increases the Picture-in-Picture size limit
-          </li>
-          <li>
-            <a href="https://github.com/itsjafer/meal-recommendations" target="_blank" rel="noopener noreferrer">
-              {' '}
-              <b>Meal Recommendations</b>
-              {' '}
-            </a>
-            {' '}
-            - An online training/prediction model that uses TF-IDF to create user profiles to store and aggregate user ingredient preferences in order to recommend new meals the user may like.
-          </li>
-          <li>
-            <a href="https://github.com/itsjafer/web-msngr" target="_blank" rel="noopener noreferrer">
-              {' '}
-              <b>web-msngr</b>
-              {' '}
-            </a>
-            {' '}
-            - a personal messaging web app made using Angular, Mongo, Node.js, and Express and deployed using Heroku
-          </li>
-          <li>
-            <a href="https://github.com/itsjafer/sparkmonitor" target="_blank" rel="noopener noreferrer">
-              <b>SparkMonitor</b>
-              {' '}
-              @ Yelp
-            </a>
-            {' '}
-            - an open-source JupyterLab extension to track progress of Spark jobs
-          </li>
-        </ul> */}
+          <SizeMe>
+              {({ size }) => (
+                <Document file={"Resume.pdf"}>
+                  <Page pageNumber={1}  onLoadSuccess={this.removeTextLayerOffset} width={size.width ? size.width : 1} />
+                </Document>
+              )}
+          </SizeMe>
+        </div>
       </div>
     );
   }
