e3f48b02138387d09f8e157a7d8320b9a71baa9e
Jafer Haider <itsjafer@gmail.com>
2021-02-09 16:05:54 +0000

    Updated trader chart with auto-update and current holdings

--- src/Components/Trader.js
+++ src/Components/Trader.js
@@ -10,14 +10,14 @@ class Trader extends Component {
     super(props);
     this.state = {
       data: null,
-      positions: null,
+      positions: null
     };
     this.updatePredicate = this.updatePredicate.bind(this);
   }
 
-  getHistoricalData() {
+  getCurrentData() {
 
-    const URI = "https://us-central1-alpacatrader-304216.cloudfunctions.net/getHistoricalPerformance";
+    const URI = "https://us-central1-alpacatrader-304216.cloudfunctions.net/getAlpacaInfo";
     fetch(`${URI}`)
     .then((response) => {
         return response.json();
@@ -28,26 +28,18 @@ class Trader extends Component {
             return [moment(item['date']).subtract(6, 'h').valueOf(), parseFloat(item['equity'])]
         })
         trimmed.sort(function(a,b) { return a[0] > b[0] })
-        this.setState({ data: trimmed });
-    })
-    .catch((e) => {
-        alert(e)
-        console.log(e);
-    });
-  }
+        const limitedPositions = data['positions'] ? data['positions'].map((position) => ({
+            'symbol': position['symbol'],
+            'profit': position['profit'],
+            'profitPercent': position['profitPercent']
+        })) : [];
 
-  getCurrentData() {
-
-    const URI = "https://us-central1-alpacatrader-304216.cloudfunctions.net/getAlpacaEquity";
-    fetch(`${URI}`)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
         this.setState({
             equity: parseFloat(data['equity']),
             change: parseFloat(data['change']),
-            positions: data['positions']
+            positions: data['positions'],
+            data: trimmed,
+            limitedPositions: limitedPositions
         })
     })
     .catch((e) => {
@@ -56,25 +48,39 @@ class Trader extends Component {
     });
   }
 
-  formatDollar(num) {
+  static formatDollar(num) {
     if (!num) {
         return;
     }
-    var p = num.toFixed(2).split(".");
-    return ["$", p[0].split("").reverse().reduce(function(acc, num, i) {
-        return num + (i && !(i % 3) ? "," : "") + acc;
-    }, "."), p[1]].join("");
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    })
+    return formatter.format(num)
   }
 
   componentDidMount() {
     this.updatePredicate();
     window.addEventListener('resize', this.updatePredicate);
-    this.getHistoricalData()
+    this.focus = true;
     this.getCurrentData()
+    this.timer = setInterval(() => {
+        this.getCurrentData()
+    }, 30*1000)
   }
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.updatePredicate);
+    clearInterval(this.timer);
+  }
+
+  onFocus = () => {
+    this.focus = true
+  }
+
+  onBlur = () => {
+    this.focus = false
   }
 
   updatePredicate() {
@@ -82,6 +88,62 @@ class Trader extends Component {
   }
 
   render() {
+    const positionColumns = [
+        {
+            Header: 'Symbol',
+            accessor: 'symbol',
+            style: { whiteSpace: 'unset' },
+        },
+        {
+            Header: 'Quantity',
+            accessor: 'qty',
+            style: { whiteSpace: 'unset' },
+        },
+        {
+            Header: 'Cost',
+            id: 'cost',
+            accessor: (d) => Trader.formatDollar(d.cost),
+            style: { whiteSpace: 'unset' },
+        },
+        {
+            Header: 'Value',
+            id: 'value',
+            accessor: (d) => Trader.formatDollar(d.value),
+            style: { whiteSpace: 'unset' },
+        },
+        {
+            Header: 'Profit',
+            id: 'profit',
+            accessor: (d) => Trader.formatDollar(d.profit),
+            style: { whiteSpace: 'unset' },
+        },
+        {
+            Header: 'Profit Percent',
+            id: 'profitPercent',
+            accessor: (d) => (d.profitPercent * 100).toFixed(2) + "%",
+            style: { whiteSpace: 'unset' },
+        },
+
+        ];
+        const limitedColumns = [
+        {
+            Header: 'Symbol',
+            accessor: 'symbol',
+            style: { whiteSpace: 'unset' },
+        },
+        {
+            Header: 'Profit',
+            id: 'profit',
+            accessor: (d) => Trader.formatDollar(d.profit),
+            style: { whiteSpace: 'unset' },
+        },
+        {
+            Header: 'Profit Percent',
+            id: 'profitPercent',
+            accessor: (d) => (d.profitPercent * 100).toFixed(2) + "%",
+            style: { whiteSpace: 'unset' },
+        },
+        ];
       const config = {
           title: {
               text: ''
@@ -94,12 +156,14 @@ class Trader extends Component {
               }
           }]
       };
-      const equity = this.state.equity;
-      const formattedEquity = this.formatDollar(equity)
-      const change = this.state.change;
-      const formattedChange = this.formatDollar(change)
+      const { equity, change, positions, isDesktop, limitedPositions } = this.state;
+      const formattedEquity = Trader.formatDollar(equity)
+      const percentChange = ((change * 100 )/ equity).toFixed(2)
+      const formattedChange = Trader.formatDollar(change)
       const overallChange = equity - 100000;
-      const formattedOverall = this.formatDollar(overallChange);
+      const overallPercent = ((overallChange * 100) / equity).toFixed(2)
+      const formattedOverall = Trader.formatDollar(overallChange);
+
     return (
       <div>
         <Helmet>
@@ -107,17 +171,32 @@ class Trader extends Component {
           <meta name="description" content="Algo trader using stock popularity on Reddit"/>
           <link rel="canonical" href="http://itsjafer.com/#/trader" />
         </Helmet>
-        <p>This page is an interface for an algorithmic day trader I'm currently paper testing. On this page, you will see the portfolio performance and current holdings (updated every 15 minutes or on refresh). The algorithm liquidates all positions at market close.</p>        
+        <p>This page is an interface for an algorithmic day trader I'm currently paper testing. On this page, you will see the portfolio performance and current holdings. This is updated every 30 seconds while this page is open and every 15 minutes otherwise. The algorithm liquidates all positions at market close.</p>        
         
         {   equity && (
             <div>
                 <h2>{formattedEquity}</h2>
-                <h4>{change >= 0 ? "+" + formattedChange : "-" + formattedChange} since last close</h4>
-                <h4>{overallChange >= 0 ? "+" + formattedOverall : "-" + formattedOverall} all time</h4>
+        <h4>{change >= 0 ? "+" + formattedChange : formattedChange} ({percentChange +"%"}) since last close</h4>
+                <h4>{overallChange >= 0 ? "+" + formattedOverall : formattedOverall} ({overallPercent +"%"}) all time</h4>
             </div>
             )
         }
         <ReactHighcharts config={config}></ReactHighcharts>
+        {
+            positions && (
+                <div>
+                <h4>Current Holdings</h4>
+
+                <ReactTable
+                data={isDesktop ? positions : limitedPositions}
+                columns={isDesktop ? positionColumns : limitedColumns}
+                defaultPageSize={positions.length}
+                showPaginationBottom={false}
+                showPageSizeOptions={false}
+                />
+                </div>
+            )
+        }
         </div>
     );
   }
