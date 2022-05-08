becba3dd460fa4b031b7525f751170edeee6aca8
Jafer Haider <itsjafer@gmail.com>
2022-05-07 22:15:59 -0500

    Update Parser.js

--- src/Components/Parser.js
+++ src/Components/Parser.js
@@ -160,6 +160,8 @@ class Parser extends Component {
         </Helmet>
 
         <p><b>How well does your resume get parsed?</b></p>
+      
+        <p><b>As of May 7 2022, this tool no longer works.</b><p>
         
         <p>
         This tool uses Lever's resume parsing API to parse resumes. Use this to see how well your resume is read by Application Tracking Systems (ATS) when applying to jobs. Companies that use Lever for job apps include: Figma, Palantir, Netflix, Twitch, Yelp and several others.
