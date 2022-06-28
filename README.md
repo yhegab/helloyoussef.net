bf67b1d4dadc0f3e38e87238e5cd8c96d2936a3c
Youssef Hegab <hegab@chapman.edu>
2022-06-27 22:02:40 -0700

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# My personal website
-
-This branch has the source code for my personal website. The website is written in ReactJS using React Router.
-
-## Deployment
-
-The website will deploy everytime a push is made to the master branch using continuous inegration with Travis CI.
-
---
+++ precache-manifest.f7c816c9e928e5d2c520ddb05260db8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250c2fcb8f3e6c1155c83e39f0425dd3",
+    "url": "/helloyoussef.net/index.html"
+  },
+  {
+    "revision": "792c03133484dc04fd5c",
+    "url": "/helloyoussef.net/static/css/2.569d7983.chunk.css"
+  },
+  {
+    "revision": "587871e75adc2a03d754",
+    "url": "/helloyoussef.net/static/css/main.6ac2bb83.chunk.css"
+  },
+  {
+    "revision": "792c03133484dc04fd5c",
+    "url": "/helloyoussef.net/static/js/2.3fcaaf6f.chunk.js"
+  },
+  {
+    "revision": "b00d50ddefe818268d972e2eccc55549",
+    "url": "/helloyoussef.net/static/js/2.3fcaaf6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "587871e75adc2a03d754",
+    "url": "/helloyoussef.net/static/js/main.7d1d6fb9.chunk.js"
+  },
+  {
+    "revision": "253cae65f005e6c21a66",
+    "url": "/helloyoussef.net/static/js/runtime-main.6626f34a.js"
+  }
+]);
